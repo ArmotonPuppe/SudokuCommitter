e79dd3607ee1ebbd32ccecfda6dfec5287de0d3b
Puppe <parkkinenjesper@gmail.com>
2025-04-14 19:56:45 +0300

    loin luokat game ja puzzle. ja attribuutteja niille

--- src/main/java/sudoku/Puzzle.java
+++ src/main/java/sudoku/Puzzle.java
@@ -1,12 +1,14 @@
 package sudoku;
 import sudoku.board.Grid;
 
+import sudoku.board.Grid;
+
 public class Puzzle {
     private Grid grid;
     private String difficulty;
     private String solution;
 
     public Puzzle() {
-
+        
     }
 }
