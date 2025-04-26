e69fc1d3084a0743a1578b667a5ca99826155608
vitun-haista <106350426+vitun-haista@users.noreply.github.com>
2025-04-26 19:53:49 +0300

    Create SudokuFonts.java

---
+++ src/main/java/sudoku/SudokuFonts.java
@@ -0,0 +1,11 @@
+package sudoku;
+
+import java.awt.Font;
+
+public class SudokuFonts {
+	public final static Font HUGE	= new Font( "Liberation Mono", Font.BOLD, 48 );
+	public final static Font LARGE	= new Font( "Liberation Mono", Font.BOLD, 32 );
+	public final static Font MEDIUM	= new Font( "Liberation Mono", 0, 14 );
+	public final static Font SMALL	= new Font( "Liberation Mono", 0, 12 );
+	public final static Font TINY	= new Font( "Liberation Mono", 0, 8 );
+}
