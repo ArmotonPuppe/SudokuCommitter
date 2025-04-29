24b62e70b861e2516ffb6d89f116b3917cc352e2
vitun-haista <106350426+vitun-haista@users.noreply.github.com>
2025-04-29 04:21:23 +0300

    Update gridpainttest.java
    rehässing was doned

--- src/main/java/sudoku/gridpainttest.java
+++ src/main/java/sudoku/gridpainttest.java
@@ -3,6 +3,7 @@ package sudoku;
 
 import java.awt.*;
 import java.awt.event.*;
+
 import javax.swing.*;
 import javax.swing.border.*;
 
@@ -10,10 +11,26 @@ import java.util.Enumeration;
 
 public class gridpainttest {
 	
-	static final int GRIDX = 9;
-	static final int GRIDY = 9;
-	static final int BLOCKX = 3;
-	static final int BLOCKY = 3;
+	static int gridx = 9;
+	static int gridy = 9;
+	static int blockx = 3;
+	static int blocky = 3;
+	
+	static final BevelBorder btnBevelRaised = new BevelBorder(BevelBorder.RAISED);
+	static final BevelBorder btnBevelLowered = new BevelBorder(BevelBorder.LOWERED);
+	static final Color btnColorRaised = new Color( 0xccffff );
+	static final Color btnColorLowered = new Color( 0x99ff99 );
+	
+	static final Font fontLarge = new Font( "Liberation Mono", Font.BOLD, 32 );
+	static final Font fontMedium = new Font( "Liberation Mono", Font.BOLD, 24 );
+	
+	static final JFrame frame = new JFrame();
+	
+	static JSpinner spinnergx, spinnergy, spinnerbx, spinnerby;
+	static ButtonGroup palButtonGroup;
+	
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
 	
 	private static String paletteSelected( ButtonGroup group ) {
 		for (Enumeration<AbstractButton> pButtons = group.getElements(); pButtons.hasMoreElements();) {
@@ -32,13 +49,6 @@ public class gridpainttest {
 		}
 		if (ignoreButton == null) { group.clearSelection(); }
 	}
-	
-	static final BevelBorder btnBevelRaised = new BevelBorder(BevelBorder.RAISED);
-	static final BevelBorder btnBevelLowered = new BevelBorder(BevelBorder.LOWERED);
-	static final Color btnColorRaised = new Color( 0xccffff );
-	static final Color btnColorLowered = new Color( 0x99ff99 );
-	
-	static final Font fontLarge = new Font( "Liberation Mono", Font.BOLD, 32 );
 	
 	static void styleButton( AbstractButton btn, boolean pressed ) {
 		if (btn == null) { return; }
@@ -59,61 +69,132 @@ public class gridpainttest {
 		public void keyPressed(KeyEvent e) {
 			AbstractButton source = (AbstractButton) e.getSource();
 			Character key = e.getKeyChar();
-			if (GRIDX < 10) {
-				if (key >= '0' && key <= '9') {
-					System.out.println( source.getActionCommand() + ": ↓" + key );
-					source.setText( (key == '0') ? "" : key.toString() );
+			
+			System.out.printf( "%s: ↓ (\"%s\",%s)\n",
+				source.getName(), key, e.getKeyCode()
+			);
+			
+			if (gridx < 10) {
+				if (key > '0' && key <= ('0'+gridx)) {
+					source.setText( key.toString() );
 				}
+			} else {
+				Character keyUp = Character.toUpperCase(key);
+				if (keyUp >= 'A' && keyUp <= ('A'+gridx-1)) {
+					source.setText( keyUp.toString() );
+				}
+			}
+			
+			if (
+					key == '0' // both 0 and numpad 0
+				||	key == 127 // delete
+				||	key == 8 // backspace
+			) {
+				source.setText( "" );
 			}
 		}
 		@Override
 		public void keyReleased(KeyEvent e) {}
-		
 	}
 	
-	static Runnable run = () -> {
-		JFrame frame = new JFrame( "grid paint test" );
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container frameCont = frame.getContentPane();
-		
-		int scrw = Toolkit.getDefaultToolkit().getScreenSize().width;
-		int scrh = Toolkit.getDefaultToolkit().getScreenSize().height;
-		
-		int winw = 128 + (GRIDX + BLOCKX) * 64;
-		int winh = 0 + (GRIDY + BLOCKY) * 64;
-		int winx = scrw/2 - winw/2;
-		int winy = scrh/2 - winh/2;
-		frame.setBounds( winx, winy, winw, winh );
-		System.out.printf( "x:%d, y:%d, w:%d, h:%d\n", winx, winy, winw, winh );
-		
-		
-		// palette kinda
-		ButtonGroup palButtonGroup = new ButtonGroup();
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+	
+	private static JPanel buildSpinners( int gx, int gy, int bx, int by ) {
+		JPanel selectPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+			selectPanel.setBackground(new Color( 0xff99cc ));
+			
+			selectPanel.add(new JLabel( "grid X" ));
+			spinnergx = new JSpinner(new SpinnerNumberModel(
+				gx, 4, 26, 1
+			));
+			selectPanel.add( spinnergx );
+			
+			selectPanel.add(new JLabel( "grid Y" ));
+			spinnergy = new JSpinner(new SpinnerNumberModel(
+				gy, 4, 26, 1
+			));
+			selectPanel.add( spinnergy );
+			
+			selectPanel.add(new JLabel( "block X" ));
+			spinnerbx = new JSpinner(new SpinnerNumberModel(
+				bx, 2, 6, 1
+			));
+			selectPanel.add( spinnerbx );
+			
+			selectPanel.add(new JLabel( "block Y" ));
+			spinnerby = new JSpinner(new SpinnerNumberModel(
+				by, 2, 6, 1
+			));
+			selectPanel.add( spinnerby );
+			
+			JButton selectButton = new JButton( "make it so" );
+				selectButton.setIcon(new ImageIcon("icons/makeitso.png"));
+				selectButton.addActionListener((ActionEvent ae) -> {
+					gridx = (int) spinnergx.getValue();
+					gridy = (int) spinnergy.getValue();
+					blockx = (int) spinnerbx.getValue();
+					blocky = (int) spinnerby.getValue();
+					
+					System.out.printf( "gx:%d, gy:%d, bx:%d, by:%d\n",
+						gridx, gridy, blockx, blocky
+					);
+					
+					frame.getContentPane().removeAll();
+					huhhahhei();
+					frame.getContentPane().validate();
+				});
+			selectPanel.add( selectButton );
+		return selectPanel;
+	}
+	
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+	
+	private static JPanel buildPalette( int num, ButtonGroup group ) {
+		Dimension btnSize = new Dimension( 32, 32 );
 		JPanel pal = new JPanel();
-			pal.setPreferredSize(new Dimension(winw/16, winh));
 			pal.setLayout(new GridLayout(0,1,0,0));
-			for (int i = 0; i < GRIDX; i++) {
-				JToggleButton palBtn = new JToggleButton( "" + (i+1) );
+			for (int i = 0; i < num; i++) {
+				JToggleButton palBtn = new JToggleButton();
+				if (num < 10) {
+					palBtn.setText( "" + (i+1) );
+				} else {
+					palBtn.setText( Character.toString('A'+i) );
+				}
+				palBtn.setFont( fontMedium );
+				palBtn.setSize(btnSize);
 				styleButton( palBtn, false );
 				
 				palBtn.addActionListener((ActionEvent ae) -> {
 					AbstractButton btn = (AbstractButton) ae.getSource();
 					if (btn.getBackground().equals(btnColorRaised)) {
 						styleButton( btn, true );
-						paletteDeselect( palButtonGroup, btn );
+						paletteDeselect( group, btn );
 					} else {
 						styleButton( btn, false );
-						paletteDeselect( palButtonGroup, null );
+						paletteDeselect( group, null );
 					}
 				});
 				
 				pal.add( palBtn );
-				palButtonGroup.add( palBtn );
+				group.add( palBtn );
 			}
+			pal.setPreferredSize(new Dimension(48, num*32));
 			pal.setBackground(new Color( 0xff0000 ));
-		frameCont.add( pal, BorderLayout.LINE_START );
-		
-		// grid
+		return pal;
+	}
+	
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+	
+	private static JPanel buildGrid(
+		int gridx,
+		int gridy,
+		int blockx,
+		int blocky,
+		ButtonGroup palGroup
+	) {
 		JPanel gridPanel = new JPanel(new BorderLayout( 16, 16 ));
 			gridPanel.setBackground(new Color( 0x99ccff ));
 			gridPanel.setBorder(
@@ -138,17 +219,17 @@ public class gridpainttest {
 				
 				c.gridx = 0;
 				c.gridy = 0;
-				for (int y = 0; y < GRIDY; y++) {
+				for (int y = 0; y < gridy; y++) {
 					
-					if (y > 0 && y % BLOCKY == 0) {
+					if (y > 0 && y % blocky == 0) {
 						c.gridwidth = GridBagConstraints.REMAINDER;
 						gridBase.add(new FillerPanel(gridBaseColor), c);
 						c.gridy++;
 					}
 					
-					for (int x = 0; x < GRIDX; x++) {
+					for (int x = 0; x < gridx; x++) {
 						c.gridwidth = 1;
-						if (x > 0 && x % BLOCKX == 0) {
+						if (x > 0 && x % blockx == 0) {
 							gridBase.add(new FillerPanel(gridBaseColor), c);
 							c.gridx++;
 						}
@@ -161,7 +242,7 @@ public class gridpainttest {
 							gbtn.setText( "" );
 							gbtn.addKeyListener(new GridPaintListener());
 							gbtn.addActionListener((ActionEvent ae) -> {
-								String ps = paletteSelected(palButtonGroup);
+								String ps = paletteSelected(palGroup);
 								if (ps != null) {
 									if (gbtn.getText().equals(ps)) {
 										gbtn.setText( "" );
@@ -178,7 +259,49 @@ public class gridpainttest {
 				}
 			}
 			gridPanel.add( gridBase, BorderLayout.CENTER );
-		frameCont.add( gridPanel, BorderLayout.CENTER );
+		return gridPanel;
+	}
+	
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+	
+	private static void huhhahhei() {
+		Container frameCont = frame.getContentPane();
+		
+		frameCont.add(
+			buildSpinners( gridx, gridy, blockx, blocky ),
+			BorderLayout.PAGE_END
+		);
+		
+		palButtonGroup = new ButtonGroup();
+		frameCont.add(
+			buildPalette( gridx, palButtonGroup ),
+			BorderLayout.LINE_START
+		);
+		
+		frameCont.add(
+			buildGrid( gridx, gridy, blockx, blocky, palButtonGroup ),
+			BorderLayout.CENTER
+		);
+		
+		frame.pack();
+		
+		int scrw = Toolkit.getDefaultToolkit().getScreenSize().width;
+		int scrh = Toolkit.getDefaultToolkit().getScreenSize().height;
+		
+		int winw = frame.getWidth();
+		int winh = frame.getHeight();
+		int winx = scrw/2 - winw/2;
+		int winy = scrh/2 - winh/2;
+		frame.setLocation( winx, winy );
+		System.out.printf( "x:%d, y:%d, w:%d, h:%d\n", winx, winy, winw, winh );
+	}
+	
+	private static final Runnable run = () -> {
+		frame.setTitle( "grid paint test" );
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		huhhahhei();
 		
 		frame.pack();
 		frame.setVisible(true);
